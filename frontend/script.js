decfeb9d4b0cdc9c881bbc35a894ad386aa75a85
Ezaan  Babar <130510334+EzaanBabar@users.noreply.github.com>
2024-04-11 20:17:44 -0500

    Add files via upload

---
+++ frontend/script.js
@@ -0,0 +1,26 @@
+function importPopup() {
+    document.getElementById("myImportPopup").style.display = "block";
+}
+
+
+function closeImportPopup() {
+    document.getElementById("myImportPopup").style.display = "none";
+}
+
+
+function infoPopup() {
+    document.getElementById("myMoreInformation").style.display = "block";
+}
+
+
+function closeInfoPopup() {
+    document.getElementById("myMoreInformation").style.display = "none";
+}
+
+
+/* function runTest() {
+    var scriptPath = "C:/Users/baseb/Desktop/IDS Workspace/Main.py";
+    subprocess.run(["python", scriptPath]);
+} *
+
+*/
