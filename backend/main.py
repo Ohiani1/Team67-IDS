60170c80d569763850de715539606eea9c19ab7c
Ohiani1 <ohianiabdulkadir7@gmail.com>
2024-04-16 19:33:19 -0500

    updated

--- backend/main.py
+++ backend/main.py
@@ -28,9 +28,10 @@ app.static_folder = os.path.join(os.path.dirname(__file__), '../Frontend/static'
 def index():
     return render_template('index.html')
 
-@app.route('/compare.html')
-def index():
+@app.route('/compare')
+def compare():
     return render_template('compare.html')
+
 
 #########################################################################################################################################
 
