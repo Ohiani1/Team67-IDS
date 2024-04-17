9c0db8710c7bfa9ae3b6cebcd33772ebdffcf939
Ohiani1 <ohianiabdulkadir7@gmail.com>
2024-04-17 05:31:12 -0500

    update

--- Frontend/static/script.js
+++ Frontend/static/script.js
@@ -19,6 +19,8 @@ function closeInfoPopup() {
 
 var run;
 
+var homeUrl = "https://cd06-153-33-34-246.ngrok-free.app"
+
 
 
 function callBackendAPI() {
@@ -29,7 +31,7 @@ function callBackendAPI() {
     document.querySelector('.runButton').textContent = selectedOption;
 
     // Construct the URL with selected option as path parameter
-    var url = 'https://d13c-70-121-35-140.ngrok-free.app/run/' + encodeURIComponent(selectedOption);
+    var url = `${homeUrl}/run/` + encodeURIComponent(selectedOption);
 
     // Send a GET request to the backend API
     fetch(url)
